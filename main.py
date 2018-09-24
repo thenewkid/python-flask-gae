b898b70d9f7b5768ffd073c79937168feaab1e1c
gurujilove108 <cyberkid108@gmail.com>
2018-09-23 19:09:45 -0500

    reset code back to default gae-flask-template

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ import time
 
 # flask app and jinja2 settings
 from google.appengine.ext import ndb
-from google.appengine.ext.ndb import GenericProperty
-
 
 def create_flask_app():
     app = Flask(__name__)
@@ -41,93 +39,7 @@ app = create_flask_app()
 
 @app.route("/")
 def index():
-    return str(BrightLandListing.search())
-
-
-@app.route("/upload_json", methods=["post"])
-def upload_json():
-    try:
-        d = request.get_json(force=True)
-        BrightLandListing.store_listings(d)
-        logging.info("\n\n")
-        return "success"
-    except Exception as e:
-        logging.info(str(e))
-        return "failure"
-
-
-class BrightLandListing(ndb.Expando):
-    ListOfficeKey = ndb.StringProperty()
-    LotSizeUnits = ndb.StringProperty()
-    ListAgentPreferredPhone = ndb.StringProperty()
-    SchoolDistrictKey = ndb.StringProperty()
-    MLSListDate = ndb.DateTimeProperty()
-    Directions = ndb.StringProperty()
-    Longitude = ndb.StringProperty()
-    PricePerSquareFoot = ndb.StringProperty()
-    SyndicationRemarks = ndb.StringProperty()
-    LostSizeAcres = ndb.StringProperty()
-    StreetName = ndb.StringProperty()
-    ListAgentMlsId = ndb.StringProperty()
-    Latitude = ndb.StringProperty()
-    AssessmentYear = ndb.StringProperty()
-    LotSizeArea = ndb.StringProperty()
-    PublicRemarks = ndb.StringProperty()
-    FullStreetAddress = ndb.StringProperty()
-    TaxYear = ndb.StringProperty()
-    ListingTaxID = ndb.StringProperty()
-    ListAgentFullName = ndb.StringProperty()
-    ListAgentEmail = ndb.StringProperty()
-    ListAgentKey = ndb.StringProperty()
-    LotSizeSquareFeet = ndb.StringProperty()
-    PostalCode = ndb.StringProperty()
-    UnparsedAddress = ndb.StringProperty()
-    ListingKey = ndb.StringProperty()
-    TaxAnnualAmount = ndb.StringProperty()
-    CloseDate = ndb.DateTimeProperty()
-    ListPrice = ndb.StringProperty()
-    SubdivisionName = ndb.StringProperty()
-    MlsStatus = ndb.StringProperty()
-    ListingId = ndb.StringProperty()
-
-    @classmethod
-    def store_listings(cls, obj):
-        new_listing = BrightLandListing()
-        exists = cls.query(cls.ListingId == obj["ListingId"]).get()
-        if not exists:
-            for key in obj.keys():
-                if key == "CloseDate" or key == "MLSListDate":
-                    if obj[key]:
-                        dt = parser.parse(obj[key])
-                        setattr(new_listing, key, dt)
-                    else:
-                        setattr(new_listing, key, None)
-                else:
-                    setattr(new_listing, key, obj[key])
-                    
-            new_listing.put()
-            logging.info("new listing stored")
-        else:
-            logging.info("listing id already exists")
-
-    @classmethod
-    def search(cls):
-        total_set = []
-        after_2016 = cls.query(cls.AssessmentYear > '2016').fetch(10)
-
-        try:
-            for obj in after_2016:
-                jsonifyable = obj.to_dict()
-                logging.info(type(jsonifyable["CloseDate"]))
-                logging.info("\n")
-                # total_set.append(json.dumps(jsonifyable))
-
-        except Exception as e:
-            logging.info(str(e))
-            return total_set
-
-        return json.dumps(total_set)
-
+    return "Hello World"
 
 
 
